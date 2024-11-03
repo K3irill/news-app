b4717ea74fdc794e527df24dee914cd698e570d2
Kirill <razdor.2019@mail.ru>
2024-11-03 22:29:59 +0300

    feat: add formatTime NewsBanner, Image, api

--- src/components/NewsBanner/NewsBanner.jsx
+++ src/components/NewsBanner/NewsBanner.jsx
@@ -1,10 +1,18 @@
-import styles from './Header.module.css'
+import styles from './NewsBanner.module.css'
+import Image from '../Image/Image'
+import { formTimeAgo } from '../../helpers/formatTimeAgo'
 
 const NewsBanner = ({ item }) => {
+	console.log(item)
+
 	return (
 		<div className={styles.banner}>
+			<Image image={item?.image} />
+
 			<h3 className={styles.title}>{item.title}</h3>
-			<p className={styles.header}></p>
+			<p className={styles.extra}>
+				{formTimeAgo(item.published)} by {item.author}
+			</p>
 		</div>
 	)
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,27 @@
 import styles from './Main.module.css'
-
+import NewsBanner from '../components/NewsBanner/NewsBanner'
+import { useEffect } from 'react'
+import { getNews } from '../api/apiNews'
+import { useState } from 'react'
 const Main = () => {
-	return <main className={styles.main}>main</main>
+	const [news, setNews] = useState([])
+	useEffect(() => {
+		const fetchNews = async () => {
+			try {
+				const response = await getNews()
+				setNews(response.news)
+			} catch (error) {
+				console.log(error)
+			}
+		}
+		fetchNews()
+	}, [])
+
+	return (
+		<main className={styles.main}>
+			{news.length > 0 ? <NewsBanner item={news[0]} /> : null}
+		</main>
+	)
 }
 
 export default Main
